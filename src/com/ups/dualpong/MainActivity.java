80bdc7b5416dc2cc12211c6ebc7b2c5b4b58ac0f
jbarreau <julien.barreau@laposte.net>
2014-02-20 13:15:21 +0100

    repair merge fail

--- src/com/ups/dualpong/MainActivity.java
+++ src/com/ups/dualpong/MainActivity.java
@@ -1,14 +1,24 @@
 package com.ups.dualpong;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.util.Log;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import com.ups.dualpong.bluetooth.BluetoothClient;
+import com.ups.dualpong.bluetooth.BluetoothServer;
+
+import java.util.Set;
 
 public class MainActivity extends Activity {
-	
+    BluetoothClient BTclient;
+    BluetoothServer BTserver;
+    BluetoothAdapter adapter;
+    BluetoothDevice device;
+
 	public String classTag = "MainActivity";
 	
 	@Override
@@ -16,6 +26,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.activity_main);
+        adapter = BluetoothAdapter.getDefaultAdapter();
 	}
 
 	@Override
@@ -29,4 +40,22 @@ public class MainActivity extends Activity {
 		Log.i(classTag, "exit app");
 		finish();
 	}
+
+    public void createGame(View view){
+        BTserver = new BluetoothServer(adapter);
+        BTserver.start();
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+        for (BluetoothDevice device : pairedDevices) {
+            //Je cherche celui dont j'ai besoin (en fait les 2 tel avec lesquels j'ai testé
+            if (device.getName().equals("HTC One S")) {
+                this.device=device;
+            }
+        }
+        BTclient = new BluetoothClient(device,adapter);
+        BTclient.start();
+    }
+
+    public void joinGame(View view){
+
+    }
 }
