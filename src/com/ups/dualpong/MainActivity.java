a395cc41b8c0f4dc765977e83d8f11591545902d
Simar Jeremy <exasky@live.fr>
2014-02-20 11:58:58 +0100

    Add portrait orientation fixed

--- src/com/ups/dualpong/MainActivity.java
+++ src/com/ups/dualpong/MainActivity.java
@@ -1,7 +1,8 @@
 package com.ups.dualpong;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,6 +10,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.activity_main);
 	}
 
